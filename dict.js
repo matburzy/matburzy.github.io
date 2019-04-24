812a1b2f9a2776c8c901ead999563383c400048c
Mateusz Burzyński <matburzy@gmail.com>
2019-04-24 14:20:44 +0200

    tlumaczenie na angielski

---
+++ dict.js
@@ -0,0 +1,179 @@
+var dict = {
+  "Aspirujący programista i prawnik w jednym. W wolnym czasie realizuję projekty związane zarówno z prawem, jak i IT. Moim celem jest reprezentowanie zawodu przyszłości - prawnika-programisty. Realizuję ciekawe projekty IT pomagające w automatyzacji pracy prawnika. ": {
+    en: "Aspiring programmer and (in the future) attorney-at-law. In my free time, I work on various projects that are law and IT-themed. I would like to lead a lawyer/programmer career. I'm currently working on various projects to automate lawyer work."
+  },
+  "O mnie": {
+    en: "About Me"
+  },
+  "Portfolio": {
+  en: "Portfolio"
+  },
+  "Kariera": {
+  en: "Career"
+  },
+  "Wykształcenie": {
+  en: "Work Experience"
+  },
+  "Umiejętności": {
+  en: "Skills"
+  },
+  "Zainteresowania": {
+  en: "Hobbies"
+  },
+  "Wyróżnienia": {
+  en: "Awards and Certificates"
+  },
+  "Przejdź do Portfolio": {
+  en: "Go to Portfolio"
+  },
+  "Pokaż informacje": {
+  en: "Show more info"
+  },
+  "Wyszukiwarka KRS": {
+  en: "Polish Company Registry Browser"
+  },
+  "Prawo pracy<br>i ubezpieczeń społecznych<br>Pytania": {
+  en: "Labor Law Exam Questions"
+  },
+  "Etyka Radcy Prawnego<br>Pytania": {
+  en: "Attorney-at-law code of conduct Questions"
+  },
+  "Kancelaria Prawna<br> radca prawny Marek Burzyński": {
+  en: "Marek Burzynski Attorney-at-law Website"
+  }, 
+  "Prawnikometr": {
+  en: "Lawyer-o-meter in Poland"
+  },
+  "oratorium": {
+  en: "gg"
+  },
+  "Aplikant radcowski": {
+	en: "Attorney-at-law trainee"
+  },
+  "Kancelaria Radcy Prawnego": {
+	  en: "Attorney-at-law Office"
+  },
+  "Aktualnie pracuję w Kancelarii Radcy Prawnego, gdzie zajmuję się m.in. obsługą przedsiębiorców, e-Sąd, KRS, przekład umów z angielskiego prawniczego na polski język prawniczy i w drugą stronę, zastępstwo procesowe.": {
+	  en: "I'm currently working at Attorney-at-law Office where my duties are rendering legal services to companies, e-Court proceedings, Polish National Court Registry proceedings, translating legal contracts from English to Polish and representing invidual clients." 
+  },
+	"październik 2015 - aktualnie": {
+		en: "October 2015 - ongoing"
+	},
+	"Student prawa": {
+		en: "Law student"
+	},
+	"Syndyk upadłości": {
+		en: "Official Receiver"
+	},
+	"W poprzedniej pracy w Kancelarii Radcy Prawnego zajmowałem się obsługą postępowań upadłościowych oraz sporządzeniem uzasadnień Syndyka o przyznaniu kwot w postępowaniu upadłościowym podmiotu gospodarczego.": {
+		en: "In my previous work in Attorney-at-law Office my duty was rendering legal services in company liquidation proceedings and writing legal documents on behalf of Official Receiver."
+	},
+	"październik 2014 - czerwiec 2015": {
+		en: "October 2014 - June 2015"
+	},
+	"Praktykant": {
+		en: "Trainee"
+	},
+	"Sąd Rejonowy w Suwałkach": {
+		en: "District Court Suwalki"
+	},
+	"Uczestniczyłem w różnych czynnościach procesowych poza Sądem, jak i na sali sądowej.": {
+		en: "My duties were assisting in Court proceedings."
+	},
+	"lipiec 2014 - lipiec 2014": {
+		en: "July 2014 - July 2014"
+	},
+	"Prokuratura Rejonowa w Suwałkach": {
+		en: "District Attorney Office"
+	},
+"W ramach praktyk studenckich zdobywałem doświadczenie uczestnicząc w czynnościach procesowych prokuratora.": {
+	en: "My duties were assisting in Attorney proceedings."
+},
+"lipiec 2013 - lipiec 2013": {
+	en: "July 2013 - July 2013"
+},
+"Radca Prawny": {
+	en: "Attorney-at-law"
+},
+"w oczekiwaniu na rezultat egzaminu zawodowego": {
+	en: "waiting for bar exam results"
+},
+"Okręgowa Izba Radców Prawnych w Gdańsku": {
+	en: "Attorney-at-Law Bar in Gdansk"
+},
+"aplikant radcowski III-go roku": {
+	en: "3rd year attorney-at-law trainee"
+},
+"styczeń 2016 - grudzień 2018": {
+	en: "January 2016 - December 2018"
+},
+"Uniwersytet Gdański": {
+	en: "University of Gdansk"
+},
+"magister prawa": {
+	en: "Masters Degree of Law"
+},
+"Praca magisterska: Instytucje zabezpieczania praw konsumenta": {
+	en: "Master's Thesis on Institutions on protecting consumer's rights."
+},
+"październik 2010 - czerwiec 2015": {
+	en: "October 2010 - June 2015"
+},
+"Prawo": {
+	en: "Law"
+},
+"Prawo cywilne": {
+	en: "Civil Law"
+},
+"Prawo gospodarcze": {
+	en: "Corporate Law"
+},
+"Ochrona danych osobowych": {
+	en: "Personal Data Protection"
+},
+"Nowe technologie": {
+	en: "New Technologies"
+},
+"e-Sąd": {
+	en: "e-Court"
+},
+"Postępowania KRS": {
+	en: "National Court Registry Proceedings"
+},
+"Zastępstwo procesowe": {
+	en: "Legal representation"
+},
+"Umowy i regulaminy": {
+	en: "Contracts and statutes"
+},
+"Najważniejsze cechy": {
+	en: "Top traits"
+},
+"Profesjonalizm, cierpliwość, skrupulatność i dążenie do celu": {
+	en: "Professionalism, patience, dilligence and reaching for goal"
+},
+"Elastyczność i niekonwencjalny sposób myślenia": {
+	en: "Flexibility and open mind"
+},
+"Szybkie uczenie się i adaptacja do zmieniających się warunków pracy": {
+	en: "Quick learning and ability to swift adaption to new work conditions"
+},
+"Praca w zespole": {
+	en: "Team Player"
+},
+"Poza programowaniem oraz prawniczeniem zajmuję się również hobbistycznie muzyką, jestem pasjonatem filmów i gier video, a także lubię wybierać się na wycieczki piesze oraz rowerowe.": {
+	en: "Besides programming and being a lawyer I also create music, I'm passionate movie goer and video game player. I like going on long walks and rock a bicycle."
+},
+"Wolne chwile spędzam na zgłębianiu wiedzy na temat nowych technologii i staram się oceniać ich przydatność i zastosowanie w codziennym życiu. Krytycznie oceniam wszelkie nowinki techniczne.": {
+	en: "I spend my free time on discovering new technologies and evaluate their appliance for everyday's life. I try to weight cons and pros of every new technology"
+},
+"Amatorsko zajmuję się również fotografią i wideofilmowaniem. Posiadam rozległą wiedzę na temat formatów kompresji cyfrowego obrazu i dźwięku oraz odrębności pomiędzy różnymi formatami. Znam różnicę pomiędzy formatem, a kontenerem ;-). Potrafię również dokonywać authoringu płyt DVD i masterowania płyt Audio CD, nie stanowi dla mnie również problemu tworzenie cyfrowej kopii analogowych formatów, takich jak kaseta magnetofonowa, czy kaseta VHS.": {
+	en: "I also a amateur photo and videographer. I have extensive knowledge of digital video compression formats and differences between them. I also know the difference between format and container ;-). I am also skilled in DVD-Video authoring and CD-Audio mastering and creating digital copies of VHS tapes"
+},
+"Wyróżnienia, Nagrody, Certyfikaty": {
+	en: "Thropies and Awards"
+},
+"Stypendium dla najlepszych studentów w roku 2014-2015 Uniwersytetu Gdańskiego": {
+	en: "Scolarship for the best students of year 2014-2015 University of Gdansk"
+},
+}
