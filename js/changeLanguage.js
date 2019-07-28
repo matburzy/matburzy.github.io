6fd36340d52b9b780c765d00cd3f0d7e7d7554fc
Mateusz Burzyński <matburzy@gmail.com>
2019-07-28 20:02:35 +0200

    Implementacja CleanCode, czyszczenie i utrzymywanie kodu.

---
+++ js/changeLanguage.js
@@ -0,0 +1,12 @@
+  function English() {
+
+        var translator = $('body').translate({
+            lang: "en",
+            t: dict
+        });
+        translator.lang("en");
+    };
+
+    function Polish() {
+        location.reload();
+    };
--- js/darkmode.js
+++ js/darkmode.js
@@ -1,9 +1,6 @@
 // DOM definitions
 var nightModeToggleButton = document.querySelector(".nightModeButton");
 var lightModeToggleButton = document.querySelector(".lightModeButton");
-var container = document.querySelector(".container");
-var para = document.querySelector(".paragraph");
-var header = document.querySelector(".header");
 var body = document.querySelector("body");
 
 
