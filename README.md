2792acbbb11e8106bfd332ee34f02e1d5f28dd21
Mateusz Burzyński <matburzy@gmail.com>
2020-01-15 22:52:15 +0100

    new

---
+++ README.md
@@ -0,0 +1,6 @@
+##
+CV napisane w  HTML5 + CSS + JS (iziModal, Bootstrap, RWD).
+
+Na stronie znajduje się prezentacja portfolio, doświadczenia zawodowego oraz życiorysu. 
+
+CV dostępne jest pod adresem https://prawo.dev.
---
+++ dict.js
@@ -0,0 +1,224 @@
+var dict = {
+  "Z zamiłowania": {
+    en: "Passion-wise"
+  },
+  "Podzielność uwagi": {
+    en: "Multitasking"
+  },
+  "Kreatywność": {
+    en: "Creativity"
+  },
+  "programista samouk, a z wykształcenia": {
+    en: "self-taught programmer and trained as a"
+  },
+  "Pasjonuję się nowymi technologiami.": {
+    en: "I'm crazy about new technologies."
+  },
+"Obecnie poszukuję pracy na stanowisku front-end": {
+  en: "I'm currently available to hire as a front-end developer"
+},
+"bądź back-end": {
+  en: "or back-end"
+},
+"W wolnym czasie realizuję projekty związane zarówno z prawem, jak i IT. Technologie i języki programowania traktują jako narzędzia do określonych celów, a nie cele same w sobie. Realizuję ciekawe projekty IT pomagające w automatyzacji pracy prawnika. Moim celem jest praca jako programista. ": {
+  en: "In my free time I create projects that are law and IT-themed. I treat technologies and programming languages as tools to reach certain goal, not the goal itself. I'm rendering interesting IT projects that automate lawyer's work. My goal is to work as a programmer."
+},
+  "prawnik ze zdanym egzaminem radcowskim.": {
+    en: "lawyer with passed attorney-at-law bar exam."
+  },
+  "O mnie": {
+    en: "About Me"
+  },
+  "Portfolio": {
+  en: "Portfolio"
+  },
+  "Kariera": {
+  en: "Career"
+  },
+  "Wykształcenie": {
+  en: "Work Experience"
+  },
+  "Umiejętności": {
+  en: "Skills"
+  },
+  "Zainteresowania": {
+  en: "Hobbies"
+  },
+  "Wyróżnienia": {
+  en: "Awards and Certificates"
+  },
+  "Przejdź do Portfolio": {
+  en: "Go to Portfolio"
+  },
+  "Pokaż informacje": {
+  en: "Show more info"
+  },
+  "Wyszukiwarka KRS": {
+  en: "Polish Company Registry Browser"
+  },
+  "Prawo pracy<br>i ubezpieczeń społecznych<br>Pytania": {
+  en: "Labor Law Exam Questions"
+  },
+  "Etyka Radcy Prawnego<br>Pytania": {
+  en: "Attorney-at-law code of conduct Questions"
+  },
+  "Kancelaria Prawna<br> radca prawny Marek Burzyński": {
+  en: "Marek Burzynski Attorney-at-law Website"
+  },
+  "Prawnikometr": {
+  en: "Lawyer-o-meter in Poland"
+  },
+  "oratorium": {
+  en: "gg"
+  },
+  "Aplikant radcowski": {
+	en: "Attorney-at-law trainee"
+  },
+  "Kancelaria Radcy Prawnego": {
+	  en: "Attorney-at-law Office"
+  },
+  "Aktualnie pracuję w Kancelarii Radcy Prawnego, gdzie zajmuję się m.in. obsługą przedsiębiorców, e-Sąd, KRS, przekład umów z angielskiego prawniczego na polski język prawniczy i w drugą stronę, zastępstwo procesowe.": {
+	  en: "I'm currently working at Attorney-at-law Office where my duties are rendering legal services to companies, e-Court proceedings, Polish National Court Registry proceedings, translating legal contracts from English to Polish and representing invidual clients."
+  },
+	"październik 2015 - aktualnie": {
+		en: "October 2015 - ongoing"
+	},
+	"Student prawa": {
+		en: "Law student"
+	},
+	"Syndyk upadłości": {
+		en: "Official Receiver"
+	},
+	"W poprzedniej pracy w Kancelarii Radcy Prawnego zajmowałem się obsługą postępowań upadłościowych oraz sporządzeniem uzasadnień Syndyka o przyznaniu kwot w postępowaniu upadłościowym podmiotu gospodarczego.": {
+		en: "In my previous work in Attorney-at-law Office my duty was rendering legal services in company liquidation proceedings and writing legal documents on behalf of Official Receiver."
+	},
+	"październik 2014 - czerwiec 2015": {
+		en: "October 2014 - June 2015"
+	},
+	"Praktykant": {
+		en: "Trainee"
+	},
+	"Sąd Rejonowy w Suwałkach": {
+		en: "District Court Suwalki"
+	},
+	"Uczestniczyłem w różnych czynnościach procesowych poza Sądem, jak i na sali sądowej.": {
+		en: "My duties were assisting in Court proceedings."
+	},
+	"lipiec 2014 - lipiec 2014": {
+		en: "July 2014 - July 2014"
+	},
+	"Prokuratura Rejonowa w Suwałkach": {
+		en: "District Attorney Office"
+	},
+"W ramach praktyk studenckich zdobywałem doświadczenie uczestnicząc w czynnościach procesowych prokuratora.": {
+	en: "My duties were assisting in Attorney proceedings."
+},
+"lipiec 2013 - lipiec 2013": {
+	en: "July 2013 - July 2013"
+},
+"Zdany egzamin radcowski": {
+	en: "Passed Attorney-at-law bar exam"
+},
+"maj 2019": {
+	en: "may 2019"
+},
+"Okręgowa Izba Radców Prawnych w Gdańsku": {
+	en: "Attorney-at-Law Bar in Gdansk"
+},
+"aplikant radcowski III-go roku": {
+	en: "3rd year attorney-at-law trainee"
+},
+"styczeń 2016 - grudzień 2018": {
+	en: "January 2016 - December 2018"
+},
+"Uniwersytet Gdański": {
+	en: "University of Gdansk"
+},
+"magister prawa": {
+	en: "Masters Degree of Law"
+},
+"Praca magisterska: Instytucje zabezpieczania praw konsumenta": {
+	en: "Master's Thesis on Institutions on protecting consumer's rights."
+},
+"październik 2010 - czerwiec 2015": {
+	en: "October 2010 - June 2015"
+},
+"Prawo": {
+	en: "Law"
+},
+"Prawo cywilne": {
+	en: "Civil Law"
+},
+"Prawo gospodarcze": {
+	en: "Corporate Law"
+},
+"Ochrona danych osobowych": {
+	en: "Personal Data Protection"
+},
+"Nowe technologie": {
+	en: "New Technologies"
+},
+"e-Sąd": {
+	en: "e-Court"
+},
+"Postępowania KRS": {
+	en: "National Court Registry Proceedings"
+},
+"Zastępstwo procesowe": {
+	en: "Legal representation"
+},
+"Umowy i regulaminy": {
+	en: "Contracts and statutes"
+},
+"Najważniejsze cechy": {
+	en: "Top traits"
+},
+"Profesjonalizm, cierpliwość, skrupulatność i dążenie do celu": {
+	en: "Professionalism, patience, dilligence and reaching for goal"
+},
+"Elastyczność i niekonwencjalny sposób myślenia": {
+	en: "Flexibility and open mind"
+},
+"Szybkie uczenie się i adaptacja do zmieniających się warunków pracy": {
+	en: "Quick learning and ability to swift adaption to new work conditions"
+},
+"Praca w zespole": {
+	en: "Team Player"
+},
+"Poza programowaniem oraz prawniczeniem zajmuję się również hobbistycznie muzyką, jestem pasjonatem filmów i gier video, a także lubię wybierać się na wycieczki piesze oraz rowerowe.": {
+	en: "Besides programming and being a lawyer I also create music, I'm passionate movie goer and video game player. I like going on long walks and rock a bicycle."
+},
+"Wolne chwile spędzam na zgłębianiu wiedzy na temat nowych technologii i staram się oceniać ich przydatność i zastosowanie w codziennym życiu. Krytycznie oceniam wszelkie nowinki techniczne.": {
+	en: "I spend my free time on discovering new technologies and evaluate their appliance for everyday's life. I try to weight cons and pros of every new technology"
+},
+"Amatorsko zajmuję się również fotografią i wideofilmowaniem. Posiadam rozległą wiedzę na temat formatów kompresji cyfrowego obrazu i dźwięku oraz odrębności pomiędzy różnymi formatami. Znam różnicę pomiędzy formatem, a kontenerem ;-). Potrafię również dokonywać authoringu płyt DVD i masterowania płyt Audio CD, nie stanowi dla mnie również problemu tworzenie cyfrowej kopii analogowych formatów, takich jak kaseta magnetofonowa, czy kaseta VHS.": {
+	en: "I also a amateur photo and videographer. I have extensive knowledge of digital video compression formats and differences between them. I also know the difference between format and container ;-). I am also skilled in DVD-Video authoring and CD-Audio mastering and creating digital copies of VHS tapes"
+},
+"Wyróżnienia, Nagrody, Certyfikaty": {
+	en: "Thropies and Awards"
+},
+"Stypendium dla najlepszych studentów w roku 2014-2015 Uniwersytetu Gdańskiego": {
+	en: "Scolarship for the best students of year 2014-2015 University of Gdansk"
+},
+"Realizowałem swoje projekty w następujących technologiach:": {
+  en: "I made my projects using following technologies:"
+},
+"Lista aplikacji przedstawionych w portfolio ma charakter przykładowy. W codziennej pracy korzystam również z": {
+	en: "The projects listed above in portofolio are exemplary. In today's work I also use short"
+},
+"a, który pomaga mi w manipulacji plikami PDF, pisaniem uzasadnień do pozwów, a także innych czynności.": {
+	en: " scripts, which help me with PDF editing, writing generate legal documents and other stuff."
+},
+"Na co dzień pracuję w Kancelarii Radcy Prawnego, gdzie zajmuję się m.in. obsługą przedsiębiorców, e-Sąd, KRS, przekład umów z angielskiego prawniczego na polski język prawniczy i w drugą stronę, zastępstwo procesowe.": {
+	en: "I am currently working at attorney-at-law office, where I am responsible for rendering legal services to legal persons, e-Court proceedings, Companies Registry proceedings, translating legal contracts from English to Polish and representing Clients before court"
+},
+"DEVELOPER NIEZALEŻNY": {
+	en: "INDEPENDENT DEVELOPER"
+},
+"W wolnych chwilach realizuję własne, niezależne projekty, które zostały opublikowane w Google Play. Realizuję pełnoprawne aplikacje, aktualnie związane z moim wykształceniem i zawodem. Jestem zainteresowany tworzeniem aplikacji różnego rodzaju. Zajmuję się również tworzeniem stron internetowych pro bono, na zlecenie znajomych, w ramach przyjacielskiej pomocy." : {
+	en: "In my free time, I develop my own independent projects which are published mainly as apps in Google Play store. My projects are mainly interconnected with my work experience and education. I am interested however in developing various projects as well. I am also making websites for my friends to help them."
+},
+"październik 2015 - obecnie" : {
+	en: "October 2015 - ongoing"
+},
+}
